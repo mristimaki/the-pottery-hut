1d4ba94ad05a04e176bd8caf98c02621d06fae11
mristimaki <rosanna.ristimaki@gmail.com>
2024-06-05 23:41:49 +0000

    Added blocks to base template

--- the_pottery_hut/settings.py
+++ the_pottery_hut/settings.py
@@ -82,6 +82,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+# remove before deploying the project
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
